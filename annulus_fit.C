6350969843527aea9c0ef554aaeb64499bf09a93
“Ingmari <ingmari.tietje@cern.chgit config --global user.email ingmari.tietje@cern.ch>
2018-02-01 14:14:40 +0100

    annulus_fit_v4.C now accepts text files as an input.

--- annulus_fit.C
+++ annulus_fit.C
@@ -33,8 +33,22 @@
 //   root > .x FitAnnulus.C
 //   root > .x FitAnnulus.C++
 
+
+// Functions : 
+Double_t g3(Double_t *x, Double_t *par); 
+Double_t fun3(Double_t *x, Double_t *par); 
+void PixToMMzoom(TH2F * h1, double xmin, double xmax, double ymin, double ymax, double zmin, double zmax); 
+
+void Show(int iRun); 
+// Global switches : 
+bool canvas1 = true;
+bool canvas2 = true; 
+bool fithisto = true;
+bool writehisto = true;  
+bool savecanvas = true; 
+
 //Global variables : 
-int RunCount=0; 
+int RunCount=-1; 
 vector<Int_t> runs;
 vector<Double_t>  ampFit;
 vector<Double_t>  ampFitout;
@@ -48,16 +62,9 @@ vector<Double_t>  x0Fit;
 vector<Double_t>  y0Fit; 
 
 
-// Functions : 
-Double_t g3(Double_t *x, Double_t *par); 
-Double_t fun3(Double_t *x, Double_t *par); 
-void PixToMMzoom(TH2F * h1, double xmin, double xmax, double ymin, double ymax, double zmin, double zmax); 
-void Show(int iRun); 
-
-bool fithisto = true;
 
 // Body of the code : 
- void annulus_fit_v4(char* list){
+ void annulus_fit(char* list){
     RunCount=0; 
     int RunNow;
     ifstream flist(list);
@@ -98,9 +105,8 @@ bool fithisto = true;
     flist.close();   
 }
 
-
-// Body of the code : 
- void Show(int iRun){
+// Function that performs and shows the fit : 
+void Show(int iRun) {
 
     // Set style : 
     gStyle->SetPalette(1);
@@ -109,56 +115,49 @@ bool fithisto = true;
     Double_t pi = 3.14159265359; 
     const Int_t npar = 10;
 
+    // Coordinates I want to zoom in on : 
+    Double_t x0 = 2.8; Double_t x1 = 13.2; 
+    Double_t y0 = 3.2; Double_t y1 = 13.6; 
+    /*Double_t x0 = 3.5; Double_t x1 = 9.; 
+    Double_t y0 = 4.5; Double_t y1 = 10.;*/
 
     // Setup Canvas : 
-    Double_t w = 1400;
+    Double_t w = 700;
     Double_t h = 700;
     TCanvas * can_histo = new TCanvas("elliptical annulus", "study of the elliptical annulus ", w, h);       // name, title, width, height. 
-    can_histo -> Divide(2,1); 
-    
+    TCanvas * can_Fit = new TCanvas("data & fit", "data & fit", 2*w, h);
+    can_histo->Divide(2,2);                                    // // create 6 pads (2 divisions along x, 1 along y). 
+    can_Fit  ->Divide(2,1); 
+
     // Get Image : 
-    char nameFile[60];
+    char nameFile[30];
     sprintf(nameFile, "../root_files/raw_data/%d_annulus.root", iRun);
     TFile* file = new TFile(nameFile, "READ"); 
-    //TH2F * histo = (TH2F*)file->Get("histo_gauss"); // How is the original histogram called? 
-    TH2F * histo = (TH2F*)file->Get("ImgFull5THDetCMOS_0");
+    TH2F * histo = (TH2F*)file->Get("ImgFull5THDetCMOS_0"); 
 
+    // Convert pixels to mm in the original image and zoom in : 
+    PixToMMzoom(histo, x0, x1, y0, y1, 0., 700.);  //void PixToMMzoom(TH2F * h1, double xmin, double xmax, double ymin, double ymax, double zmin, double zmax); 
 
-    // Declaration and definition of the parameters x0, x1, y0, y1 
-    // Can I here limit the range of the fit function? 
-    /*Double_t x0 = 400.; 
-    Double_t x1 = 750.;
-    Double_t y0 = 450.; 
-    Double_t y1 = 750.; */
-
-    Double_t x0 = 0.; 
-    Double_t x1 = 1148.;
-    Double_t y0 = 0.; 
-    Double_t y1 = 1148.; 
-
-    //histo -> GetXaxis() -> SetRangeUser(x0, x1);                                       // Set zAxis to some value you like!!! 
-    //histo -> GetYaxis() -> SetRangeUser(y0, y1);
-
-
-
+    // Prepare the fit function : 
     TF2 *f3 = new TF2("f3", fun3, x0, x1, y0, y1, npar); 
-
-    /*
-    Double_t paramsf3[npar] = {10000. , 105. , 90., 0.3 , 10. , 109. , 75. , 575. ,610. , 10000. }; 
-    //amp_out[0]   , a[1] ,  b[2]     , theta[3]    , kbT[4]   , r_out[5], r_in[6], x0[7]  , y0[8], amp_in[9] 
-    f3 -> SetParameters(paramsf3); 
-    f3->SetParNames("amp_out[0]", "a[1]", "b[2]", "theta[3]", "kbT[4]","r_out[5]", "r_in[6]", "x0[7]", "y0[8]", "amp_in[9]"); 
-*/
-
     Double_t paramsf3[npar] = {10000. , 10.5/1.1 , 9.0/1.1,   0.3        ,  20.   ,   109.    , 75.     , 575.    , 610.   , 10000. }; 
-                      //amp_out[0]    , a[1] , b[2], theta[3]    , kbT[4]   , r_out[5], r_in[6] , x0[7]   , y0[8]  , amp_in[9] 
-    f3 -> SetParameters(paramsf3); 
-    f3->SetParNames("amp_out[0]", "a[1]", "b[2]", "theta[3]", "kbT[4]","r_out[5]", "r_in[6]", "x0[7]", "y0[8]", "amp_in[9]"); 
+   
+    //Double_t paramsf3[npar] =
+    //{10000.,              2.4,   2.0,            0.3,         0.2        , 1.2        , 1.1      , 6.4    , 6.7    , 10000.     }; //2*pi/180*60.
+    //amp_out[0]   , a[1] ,  b[2]     , theta[3]    , kbT[4]   , r_out[5], r_in[6], x0[7]  , y0[8], amp_in[9] 
+    // Fuer die groesseren Ringe : {10000., 1.7, 1.05, 0.3, 0.2, 1.6, 1.5, 6.15, 7.5, 10000.}; //2*pi/180*60.
+    //amp_out[0]   , a[1] ,b[2]     , theta[3]    , kbT[4]   , r_out[5], r_in[6], x0[7]  , y0[8], amp_in[9]   
 
-    //Fit the histogram with the Puck function f3. 
-    histo -> Fit("f3", "MR0"); 
+    // Set the initial parameters : 
+    f3->SetParameters(paramsf3);
 
-    // Retrieve the parameters from the fit function : 
+    if(fithisto){
+        f3->SetParNames("amp_out[0]", "a[1]", "b[2]", "theta[3]", "kbT[4]","r_out[5]", "r_in[6]", "x0[7]", "y0[8]", "amp_in[9]"); 
+
+        // Fit histogram  with function f3 : 
+        histo->Fit("f3", "0");
+
+        // Retrieve the parameters from the fit function : 
         ampFit.push_back(f3 -> GetParameter(0));
         ampFitout.push_back(f3 -> GetParameter(9)); 
         a_Fit.push_back(f3->GetParameter(1)); 
@@ -169,32 +168,56 @@ bool fithisto = true;
         r_inFit.push_back(f3->GetParameter(6)); 
         x0Fit.push_back(f3->GetParameter(7)); 
         y0Fit.push_back(f3->GetParameter(8)); 
-
+    }  
+    // Set the function f3 with the parameters returned from the fit : 
     Double_t fitPass[npar] =
     {ampFit[RunCount], a_Fit[RunCount], b_Fit[RunCount], thetaFit[RunCount], kbT_Fit[RunCount],
     r_outFit[RunCount],r_inFit[RunCount], x0Fit[RunCount], y0Fit[RunCount], ampFitout[RunCount]};
     f3->SetParameters(fitPass);
+        if(canvas1){
+            can_histo->cd(1);  histo->Draw("contour2"); 
+            can_histo->cd(2);  histo->Draw("surf2"); 
+            gPad -> Modified(); gPad -> Update(); 
+            gPad -> GetView() -> TopView(); 
+        if(fithisto){
+            can_histo->cd(3);  histo->Draw("contour2");
+            f3->Draw("same"); 
+            can_histo->cd(4);  histo->Draw("surf2");  
+            f3->Draw("same");   
+            gPad -> Modified(); gPad -> Update(); 
+            gPad -> GetView() -> TopView(); 
 
-
-
-    // Draw image: 
-    can_histo -> cd(1); 
-    histo -> Draw("surf2"); 
-    gPad -> Modified(); gPad -> Update(); 
-    gPad -> GetView() -> TopView();
-    can_histo -> cd(2); 
-    histo -> Draw("surf2"); 
-
-    //Draw fit: 
-    can_histo -> cd(1);
-    f3->Draw("same");
-
-    can_histo -> cd(2); 
-    f3->SetNpx(100);
-    f3->SetNpy(100);
-    f3->SetFillColorAlpha(5, 0.3); 
-    f3->Draw("surf2 same"); 
-
+        }
+        if(savecanvas){
+            char name_png[30];
+            sprintf(name_png,"../root_files/rotated_coord_trans/png_files/%d_surf2_top_view_v2.png", iRun);
+            can_histo -> Modified();   can_histo -> Update();
+            can_histo -> SaveAs(name_png);
+        }
+        }
+        if(canvas2){
+            can_Fit -> cd(1); histo->Draw("surf2");  
+            can_Fit -> cd(2);  
+            f3->SetNpx(100);
+            f3->SetNpy(100);
+            f3->SetFillColorAlpha(5, 0.3); 
+            histo->Draw("surf2");
+            f3->Draw("surf2 same"); 
+            if(savecanvas){
+                char name1_png[30];
+                sprintf(name1_png,"../root_files/rotated_coord_trans/png_files/%d_SurfacePlotDataAndFit.png", iRun);
+                can_Fit -> Modified();   can_Fit -> Update();
+                can_Fit -> SaveAs(name1_png);
+            }
+        }
+        if(writehisto){
+            char name[30];
+            sprintf(name, "../root_files/rotated_coord_trans/%d_annulusFit.root", iRun);
+            TFile* file1 = new TFile( name, "RECREATE");  
+            histo -> Write(); 
+            file1 -> Close(); 
+            delete file1; 
+        }
 }
 
 
